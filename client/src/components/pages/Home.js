47e4598aaed2f1d5cf0fad6a0b97263e9e5d3d1a
Tyler Austin <tyler@webguyty.com>
2020-10-05 17:06:37 -0700

    home to dashboard reroute

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated } = authContext;
+
+  useEffect(() => {}, []);
+
+  if (isAuthenticated) {
+    return (
+      <Route>
+        <Redirect to='/dashboard' />
+      </Route>
+    );
+  }
+
   return (
     <div className='home__page'>
       <div className='row'>
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -5,26 +5,26 @@ const Login = () => {
     <div className='login__page'>
       <div className='row'>
         <form action='' className='col s12 m8 offset-m2'>
-          <div class='row'>
-            <div class='input-field col s12'>
-              <input id='username' type='text' class='validate' />
+          <div className='row'>
+            <div className='input-field col s12'>
+              <input id='username' type='text' className='validate' />
               <label htmlFor='username'>Username</label>
             </div>
           </div>
-          <div class='row'>
-            <div class='input-field col s12'>
-              <input id='password' type='password' class='validate' />
+          <div className='row'>
+            <div className='input-field col s12'>
+              <input id='password' type='password' className='validate' />
               <label htmlFor='password'>Password</label>
             </div>
           </div>
           <div className='row center-align'>
             <button
-              class='btn waves-effect waves-light'
+              className='btn waves-effect waves-light'
               type='submit'
               name='action'
             >
               Submit
-              <i class='material-icons right'>send</i>
+              <i className='material-icons right'>send</i>
             </button>
           </div>
         </form>
