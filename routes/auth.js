b9857631ac8ddeaed71d6c2008c9ff721fecc330
Tyler Austin <tyler@webguyty.com>
2020-10-07 13:39:38 -0700

    get logged in private route in auth

--- routes/auth.js
+++ routes/auth.js
@@ -11,15 +11,15 @@ const User = require('../models/User');
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-// router.get('/', auth, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select('-password');
-//     res.json(user);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route		POST api/auth
 // @desc		Auth user and get token
