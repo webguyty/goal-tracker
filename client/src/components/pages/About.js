77a49692d182a72aa0bad1f0f59064b119dae732
Tyler Austin <tyler@webguyty.com>
2020-10-12 23:38:58 -0700

    materialize fix about page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,14 +4,8 @@ const About = () => {
   return (
     <div className='about__page'>
       <div className='row'>
-        <div className='col sm12'>
+        <div className='col s12'>
           <h2 className='center-align'>About</h2>
-          <div className='row'>
-            <div className='col s12 m8 offset-m2'>
-              <h3>Basic Login using MERN and materialize css</h3>
-              <p>Read Readme to learn more</p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
