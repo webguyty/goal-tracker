4ed7a063bea710778a2c34aad9feb40f80fd7ca2
Tyler Austin <tyler@webguyty.com>
2020-10-22 13:13:03 -0700

    prepare to deploy

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/dailyGoals', require('./routes/dailyGoals'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
